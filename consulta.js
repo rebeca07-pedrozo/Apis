e24d31d3009654ba6c16f1740d6798467fa7926d
Rebeca Pedrozo <rebecapedrozocueto@gmail.com>
2025-03-30 18:34:32 -0500

    Apis

--- consulta.js
+++ consulta.js
@@ -1,25 +1,25 @@
-const fs = require('fs');
+async function consulta() { 
 
-fs.readFile('archivo.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error al leer el archivo', err);
-    return;
-  }
-  const jsonData = JSON.parse(data);
-  console.log(jsonData);
-});
-async function consulta() {
-    try {
-        const response = await fetch('https://www.datos.gov.co/resource/kfsz-ija2.json?departamento=cundinamarca'); 
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        const data = await response.json();
-        console.log(data);
-        const jsonData = JSON.parse(data);
-        console.log(jsonData);
-    } catch (error) {
-        console.error('Error en la consulta:', error);
-    }
-}
-consulta();
+  try { 
+
+      const response = await fetch('https://www.datos.gov.co/resource/fvq4-wwtz.json');  
+
+      if (!response.ok) { 
+
+          throw new Error(`Error: ${response.status} ${response.statusText}`); 
+
+      } 
+
+      const data = await response.json(); 
+
+      console.log(data);  
+
+  } catch (error) { 
+
+      console.error('Error en la consulta:', error); 
+
+  } 
+
+} 
+
+consulta(); 
